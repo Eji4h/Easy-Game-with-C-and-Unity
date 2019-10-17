0dbc183b2e728d0d78224d3ea789c51c333056fe
M <menstood@gmail.com>
2019-10-18 00:50:33 +0700

    [ADD] Score Label

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+	[SerializeField]
+	private Text scoreLabel;
+
+	public void SetScore(int val)
+	{
+		scoreLabel.text = val.ToString();
+	}
+
+}
